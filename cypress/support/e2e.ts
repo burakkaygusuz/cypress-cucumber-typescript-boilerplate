315adffed028ee5cdf690dc8121cb6ad9c7b6d16
Burak Kaygusuz <kaygusuzburak@gmail.com>
2023-11-27 21:29:11 +0300

    Remove type

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -7,7 +7,7 @@ Cypress.on('uncaught:exception', (err: Error, runnable: Mocha.Runnable) => {
 
 // Hide fetch/XHR requests from command log
 if (Cypress.config('hideXHRInCommandLog')) {
-  const app: Window | null = window.top;
+  const app: Window = window.top;
   if (app && !app.document.head.querySelector('[data-hide-command-log-request]')) {
     const style: HTMLStyleElement = app.document.createElement('style');
     style.innerHTML = '.command-name-request, .command-name-xhr { display: none }';
